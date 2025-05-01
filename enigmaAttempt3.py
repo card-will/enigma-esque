36fc6e63c7ce564a16eb2f94e92e4d89ab06a153
card-will <134020504+card-will@users.noreply.github.com>
2025-05-01 13:28:51 -0700

    Update enigmaAttempt3.py

--- enigmaAttempt3.py
+++ enigmaAttempt3.py
@@ -1,3 +1,4 @@
+#third attempt in total. I finally understood the rotors after 2 other attempts and countless diagrams haha
 alpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 plugboard = alpha[:]
 rotor1 = alpha[:]
